461f9690608e4fd9baaa090e68e93cb49810f6a7
Abhishek <abhishek.kumar@mendix.com>
2024-03-05 14:11:56 +0100

    add files main.tf and outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "instance_id" {
+    description = "ID of EC2 Instance"
+    value = aws_instance.app_server.id
+  
+}
+
+output "instance_public_ip" {
+    description = "public ip of EC2 Instance"
+    value = aws_instance.app_server.public_ip
+  
+}
